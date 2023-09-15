e2b2d5e57330cf6df0a32bcb52a7fd359c9ae074
infiniti008 <nikitenok.sl@gmail.com>
2023-09-15 09:55:45 +0200

    CN-293: Done - One line tooltip

--- content-manager/src/components/Charts/RealTimeHorizontalStreamChart/CustomTooltip.jsx
+++ content-manager/src/components/Charts/RealTimeHorizontalStreamChart/CustomTooltip.jsx
@@ -1,85 +1,117 @@
 import '../../../assets/css/Tooltipe.scss'
 
+const thinChars = ['.', ',', ';', ':']
+
 function CustomTooltip({ currencyKey, dataSet, firstPoint, lastPoint, colorRGB, currentSettedPoint}) {
 
   let currentIndex = dataSet.indexOf(currentSettedPoint)
   const currentPoint = currentSettedPoint
   const beforeCurrent = dataSet?.[currentIndex - 1] || dataSet?.[0]
 
-  const currentDiff = (currentPoint?.y - beforeCurrent?.y).toFixed(4)
+  const currentDiff = (currentPoint?.y - beforeCurrent?.y)
   const currentDiffDirection = currentDiff > 0 ? 'up' : currentDiff < 0 ? 'down' : 'plate'
-  const currentDiffText = currentDiff > 0 ? `+${currentDiff}` : currentDiff
-  const currentDiffTitle = currentDiff > 0 ? '⬆' : currentDiff < 0 ? '⬇' : ''
+  const currentDiffTitle = currentDiff > 0 ? '⬆' : currentDiff < 0 ? '⬇' : ' '
 
-  const startEndDiff = (lastPoint?.y - firstPoint?.y).toFixed(4)
+  const startEndDiff = (lastPoint?.y - firstPoint?.y)
   const startEndDiffDirection = startEndDiff >= 0 ? 'up' : startEndDiff < 0 ? 'down' : 'plate'
-  const startEndDiffText = startEndDiff > 0 ? `+${startEndDiff}` : startEndDiff
-  const startEndDiffTitle = startEndDiff >= 0 ? '⬆' : startEndDiff < 0 ? '⬇' : ''
+  const startEndDiffTitle = startEndDiff >= 0 ? '⬆' : startEndDiff < 0 ? '⬇' : ' '
+
+  function getCurrencyHTML(value, addDirection) {
+    if (typeof value === 'number') {
+      const valueToFixed = value?.toFixed(4) || ''
+      const valueWithDirection = addDirection && value > 0 ? `+${valueToFixed}` : valueToFixed
+
+      const splittedValue = valueWithDirection.split('')
+      const firstPart = splittedValue.slice(0, -2).map(char => thinChars.includes(char) ? `<tc>${char}</tc>` : `<wc>${char}</wc>`)
+      const lastpart = splittedValue.slice(-2).map(char => thinChars.includes(char) ? `<tc>${char}</tc>` : `<wc>${char}</wc>`)
+
+      const valueWithEm = `<em>${firstPart.join('')}</em>${lastpart.join('')}`
+      const newValue = valueWithEm
+      
+      return newValue
+    }
+
+    return ''
+  }
+
+  function getTimeHTML(time) {
+    if (typeof time == 'string') {
+      const newTime = time.split('').map(char => thinChars.includes(char) ? `<tc>${char}</tc>` : `<wc>${char}</wc>`)
+
+      return newTime.join('')
+    }
+
+    return ''
+  }
 
   return (
     <div className="tooltip-wrapper">
       <div className="tooltip tooltip__real-time--horizontal">
-        <div className='tooltip__item'>
-          <span className='tooltip__item--top tooltip__item--label'>
-            {firstPoint.x}
-          </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {firstPoint.y?.toFixed(4)}
-          </span>
+        <div className="tooltip__group--left">
+
+            <span 
+              className='tooltip__item--time tooltip__item--start-time'
+              dangerouslySetInnerHTML={{ __html: getTimeHTML(firstPoint.x) }}
+            />
+
+            <span 
+              className='tooltip__item--start-value'
+              dangerouslySetInnerHTML={{ __html: getCurrencyHTML(firstPoint.y, false) }}
+            />
+
+            <span className={'tooltip__item--arrow' + ' tooltip__item--' + startEndDiffDirection}>
+              <em>
+                {startEndDiffTitle}
+              </em>
+            </span>
+
+            <span 
+              className={'tooltip__item--start-end-diff' + ' tooltip__item--' + startEndDiffDirection}
+              dangerouslySetInnerHTML={{ __html: getCurrencyHTML(startEndDiff, true) }}
+            />
+
+            <span 
+              className='tooltip__item--end-value'
+              dangerouslySetInnerHTML={{ __html: getCurrencyHTML(lastPoint.y, false) }}
+            />
+
+            <span 
+              className='tooltip__item--time tooltip__item--end-time'
+              dangerouslySetInnerHTML={{ __html: getTimeHTML(lastPoint.x) }}
+            />
+
         </div>
 
-        <div className={'tooltip__item tooltip__item--start-end-diff ' + 'tooltip__item--' + startEndDiffDirection}>
-          <span className='tooltip__item--top tooltip__item--label'>
-            {startEndDiffTitle}
-          </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {startEndDiffText}
-          </span>
-        </div>
-
-        <div className='tooltip__item'>
-          <span className='tooltip__item--top tooltip__item--time'>
-            {lastPoint.x}
-          </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {lastPoint.y?.toFixed(4)}
-          </span>
-        </div>
-
-        <div className='tooltip__item'>
-          <span className='tooltip__item--top tooltip__title--label' style={{color: currencyKey.bankColor}}>
+        <div className="tooltip__group--center">
+          <span className='tooltip__title--label' style={{color: currencyKey.bankColor}}>
             {currencyKey.name}
           </span>
-          <span className='tooltip__item--bottom tooltip__title--value' style={{color: `rgb(${colorRGB})`}}>
+          <span className='tooltip__title--value' style={{color: `rgb(${colorRGB})`}}>
             {currencyKey.currency} / {currencyKey.currencyBase}
           </span>
         </div>
 
-        <div className='tooltip__item'>
-          {/* <span className='tooltip__item--top tooltip__item--label'>
-            Min
-          </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {min}
-          </span> */}
-        </div>
+        <div className="tooltip__group--right">
+          <span 
+            className={'tooltip__item--time tooltip__item--current-time'}
+            dangerouslySetInnerHTML={{ __html: getTimeHTML(currentPoint.x) }}
+          />
 
-        <div className={'tooltip__item tooltip__item--current-diff ' + 'tooltip__item--' + currentDiffDirection}>
-          <span className='tooltip__item--top tooltip__item--label'>
-            {currentDiffTitle}
+          <span className={'tooltip__item--arrow' + ' tooltip__item--' + currentDiffDirection}>
+            <em>
+              {currentDiffTitle}
+            </em>
           </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {currentDiffText}
-          </span>
-        </div>
 
-        <div className={'tooltip__item tooltip__item--current ' + 'tooltip__item--' + currentDiffDirection}>
-          <span className='tooltip__item--top tooltip__item--label'>
-          {currentPoint?.x}
-          </span>
-          <span className='tooltip__item--bottom tooltip__item--currency'>
-            {currentPoint?.y?.toFixed(4)}
-          </span>
+          <span 
+            className={'tooltip__item--current-diff-text' + ' tooltip__item--' + currentDiffDirection}
+            dangerouslySetInnerHTML={{ __html: getCurrencyHTML(currentDiff, true) }}
+          />
+
+          <span 
+            className={'tooltip__item--current-value' + ' tooltip__item--' + currentDiffDirection}
+            dangerouslySetInnerHTML={{ __html: getCurrencyHTML(currentPoint.y, false) }}
+          />
         </div>
         
       </div>
